e6a4f86499214267ff8af20222a744e9f5729cce
Mohammad Akbarzadeh <moho.akbarzadeh@gmail.com>
2025-10-23 23:29:22 +0330

    feat: update route directory

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,9 @@ Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [\App\Http\Controllers\Api\Auth\AuthController::class, 'logout']);
 });
 
-Route::get('/products/search', [\App\Http\Controllers\ProductController::class, 'search']);
-Route::get('/products/recommendations', [\App\Http\Controllers\ProductController::class, 'recommendations']);
-Route::resource('/products', \App\Http\Controllers\ProductController::class)->only('show');
+Route::get('/products/search', [\App\Http\Controllers\Api\Product\ProductController::class, 'search']);
+Route::get('/products/recommendations', [\App\Http\Controllers\Api\Product\ProductController::class, 'recommendations']);
+Route::resource('/products', \App\Http\Controllers\Api\Product\ProductController::class)->only('show');
 
 Route::resource('/categories', \App\Http\Controllers\Api\Product\CategoryController::class)->only('index');
 Route::resource('/brands', \App\Http\Controllers\BrandController::class)->only('index');
@@ -32,8 +32,7 @@ Route::prefix('carts')->middleware('auth:api')->group(function () {
     Route::get('', [\App\Http\Controllers\Api\Checkout\Cart\CartController::class, 'index']);
     Route::get('items-count', [\App\Http\Controllers\Api\Checkout\Cart\CartController::class, 'itemsCount']);
 
-    // api/carts/{cart}
-    Route::prefix('{cart}')->group(function () {
-        Route::patch('products/{product}/changeCount', [\App\Http\Controllers\Api\Checkout\Cart\CartProductController::class, 'changeCount']);
-    });
+    Route::post('products/{product}', [\App\Http\Controllers\Api\Checkout\Cart\CartProductController::class, 'addToCart']);
+    Route::delete('products/{product}', [\App\Http\Controllers\Api\Checkout\Cart\CartProductController::class, 'deleteFromCart']);
+    Route::patch('products/{product}/changeCount', [\App\Http\Controllers\Api\Checkout\Cart\CartProductController::class, 'changeCount']);
 });
